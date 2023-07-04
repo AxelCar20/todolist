c1ac5e3bfa0b2d53f71ea88439b1e93ba2b73d4d
AxelCar20 <axelaaronc@gmail.com>
2023-07-03 20:33:29 -0600

    clase  completada

--- script.js
+++ script.js
@@ -5,8 +5,32 @@ const btnCreate =  document.querySelector("[data-form-btn]");
 const inputText = document.querySelector("[data-form-input]");
 const createTask = (evento) =>{
     evento.preventDefault(); //EVENTO PARA QUE NO SE REFRESQUE LA PÁGINA
-    console.log(inputText.value);
-}
+    const value = inputText.value;
+    inputText.value = "";
+    const list = document.querySelector("[data-ul-task]");
+    const task = document.createElement("li"); //SE RECIBE EL ELEMENTO QUE SE REQUIERE CREAR 
+    task.classList.add("card");
+    //SE LLAMAN BACKTICKS 
+    //SE CREAN TEMPLATES QUE JS PERMITE
+    const content = `
+                    <div>
+                    <i class="far fa-check-square icon"></i>
+                    <span class="task"> ${value} </span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`
+
+    inputText.value = "";
+
+    task.innerHTML = content;
+    list.appendChild(task);
+    //SE LE AGREGA CÓDIGO HTML
+
+    console.log(value);
+};
+/*
+<li class="card" data-li-task>      
+</li>*/
+            
 
 console.log(btnCreate);
 
